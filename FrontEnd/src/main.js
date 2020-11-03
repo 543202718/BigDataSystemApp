b4e7a8c12d178d3eb75527cc3ee23d15f0338a63
Sunny-Island <970058803@qq.com>
2020-11-03 13:26:38 +0800

    add vuex module

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -14,14 +14,14 @@ import VueResource from 'vue-resource'
 import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import Vuex from 'vuex'
+import store from './store/store.js'
+
 Vue.use(VueElementExtends)
 Vue.use(Elements)
 Vue.use(VueRouter)
 Vue.use(VueResource)
 Vue.use(VCharts)
 Vue.use(ElementUI);
-Vue.use(Vuex);
 
 const router = new VueRouter({
     mode: 'history',
@@ -35,6 +35,7 @@ new Vue({ /* Vue实例化 */
     el: '#app',
     /* 将所有视图放在id值为app这个dom元素中 */
     router,
+    store,
     /* 表明引入的文件，即上述的App.vue文件，这个文件的内容将以<App/>这样的标签写进去#app中 */
     components: { App },
     /* 告知当前页面想使用App这个组件 */
---
+++ FrontEnd/src/store/store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    /*
+    state: {
+        isLoggedIn: false,
+        account: '',
+    },
+    mutations: {
+        login(state, account) {
+            state.isLoggedIn = true;
+            state.account = account;
+        },
+        logout(state) {
+            state.isLoggedIn = false;
+            state.account = '';
+        },
+    },
+    actions: {
+
+    }
+    */
+})
