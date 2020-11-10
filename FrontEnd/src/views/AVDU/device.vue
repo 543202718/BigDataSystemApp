f70f089dd726aee3b0688386aa280998f084a62a
ttttt1216 <tzsy20@mails.tsinghua.edu.cn>
2020-11-10 18:54:57 +0800

    主要设备和公用工程

--- FrontEnd/src/views/AVDU/device.vue
+++ FrontEnd/src/views/AVDU/device.vue
@@ -1,31 +1,38 @@
 <template>
-<div>
-    <el-form ref="form" :model="deviceInfo" label-width="220px" size="mini" style="margin-top: 20px">
-       <el-form-item label="设备类型" style="text-align:left">
-            <el-select v-model="deviceInfo.type" filterable allow-create>
-                <el-option label="塔器" value="塔器"></el-option>
-                <el-option label="冷换设备" value="冷换设备"></el-option>
-                <el-option label="空冷器" value="空冷器"></el-option>
-                <el-option label="容器" value="容器"></el-option>
-            </el-select>
-            </el-form-item>
-        <el-form-item label="国内进货">
-            <el-input v-model="deviceInfo.internal" placeholder="此处填入国内进货台数"></el-input>
-        </el-form-item>
-        <el-form-item label="国外进货">
-            <el-input v-model="deviceInfo.overseas" placeholder="此处填入国外进货台数">
-            </el-input>
-        </el-form-item>
-        <el-form-item label="备注">
-            <el-input v-model="deviceInfo.note" placeholder="填入备注内容"></el-input>
-        </el-form-item>
-        <!--
-        <el-form-item size="large">
-            <el-button type="primary" @click="onSubmit">立即创建</el-button>
-            <el-button>取消</el-button>
-        </el-form-item>
--->
-    </el-form>
+<div id="hello">
+    <h4 style="display: inline-block;margin:0;">测试表</h4>
+    <div style="display: inline-block;float: right;">
+        <el-button size="mini" type="primary" @click="consoleDatas">打印数据</el-button>
+        <el-button size="mini" type="primary" @click="addRow">增加行</el-button>
+
+        <el-button @click="delLastRow" slot="reference" type="primary" size="mini">删除末行</el-button>
+
+    </div>
+    <el-table :data="testDatas" border style="width: 100%;margin-top:10px" @header-contextmenu="colRightClick">
+        <el-table-column v-if="testCols.length > 0" type="index" :label="'编号'" :width="50"></el-table-column>
+        <el-table-column v-for="(column, idx) in testCols" :key="idx" :index="idx">
+            <!--label-->
+            <template slot="header" slot-scope="scope1">
+                <p v-show="column.show" @dblclick="column.show=false">
+                    {{column.txt}}
+                    <i class="el-icon-edit-outline" @click="column.show=false"></i>
+                </p>
+                <el-input size="mini" v-show="!column.show" v-model="column.txt" @blur="column.show=true">
+                </el-input>
+            </template>
+            <!--prop-->
+            <template slot-scope="scope">
+                <p v-show="scope.row[column.col].show" @dblclick="scope.row[column.col].show=false">
+                    {{scope.row[column.col].content}}
+                    <i class="el-icon-edit-outline" @click="scope.row[column.col].show=false"></i>
+                </p>
+                <el-input type="textarea" :autosize="{minRows:2,maxRows:4}" v-show="!scope.row[column.col].show" v-model="scope.row[column.col].content" @blur="scope.row[column.col].show=true">
+                </el-input>
+            </template>
+        </el-table-column>
+    </el-table>
+
+
 </div>
 </template>
 
@@ -33,20 +40,117 @@
 export default {
     data() {
         return {
-            deviceInfo: {
-                type:'设备类型',
-                internal:'国内订货数',
-                overseas:'国外订货数',
-	            note:'备注', 
-            },
-        };
-
+            testCols: [
+                { col: "type", txt: '项目', show: true },
+                { col: "internal", txt: '国内订货/台数', show: true },
+                { col: "overseas", txt: '国外订货/台数', show: true },
+                { col: "note", txt: '备注', show: true }
+            ],
+            testDatas: [{
+                type: { content: '塔器', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            }, {
+                type: { content: '冷换设备', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                type: { content: '空冷器', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                type: { content: '容器', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                type: { content: '机泵', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                type: { content: '其他机械设备', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                type: { content: '加热炉', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                type: { content: '热工设备', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                type: { content: '压缩机', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                type: { content: '电脱盐成套设备', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                type: { content: '机械抽空器', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                type: { content: '其它', show: true },
+                internal: { content: '', show: true },
+                overseas: { content: '', show: true },
+                note: { content: '', show: true }
+            }],
+            count_col: 0,
+            showMenu: false,
+            curColumn: null,
+            //systemInfo:{
+            //    .....
+            //}
+        }
     },
-
     created: function () {
         console.log("turn to system page");
     },
-    methods: {},
+    methods: {
+        colRightClick(column, event) {
+            window.event.returnoverseas = false; //阻止浏览器自带的右键菜单弹出
+            if (!column.index && column.index !== 0) return;
+            this.curColumn = column.index
+            this.showMenu = true
+            var ele = document.getElementById('contextmenu')
+            ele.style.top = event.clientY + 'px';
+            ele.style.left = event.clientX + 'px';
+            if (window.innerWidth - 140 < event.clientX) {
+                ele.style.left = 'unset'
+                ele.style.right = 0
+            }
+        },
+        addRow() { // 新增行
+            this.showMenu = false
+            var obj = {}
+            this.testCols.map(p => {
+                obj[p.col] = { content: '', show: true }
+            })
+            this.testDatas.push(obj)
+        },
 
+        delLastRow() { // 删除行
+            this.showMenu = false
+            var len = this.testDatas.length;
+            if (len > 0) this.testDatas.splice(len - 1, 1);
+            else this.$message.error('没有可删除行');
+        },
+        consoleDatas() {
+            console.log('表头', this.testCols);
+            console.log('数据', this.testDatas);
+        }
+    }
 }
 </script>
--- FrontEnd/src/views/AVDU/public_work.vue
+++ FrontEnd/src/views/AVDU/public_work.vue
@@ -1,143 +1,186 @@
 <template>
-  <div id="hello">	
-		<h4 style="display: inline-block;margin:0;">测试表</h4>
-		<div style="display: inline-block;float: right;">
-			<el-button size="mini" type="primary" @click="consoleDatas">打印数据</el-button>
-			<el-button size="mini" type="primary" @click="addRow">增加行</el-button>
-			<el-popconfirm title="确定删除最后行吗？" @onConfirm="delLastRow">
-				<el-button slot="reference" type="primary" size="mini">删除末行</el-button>
-			</el-popconfirm>
-		</div>
-<el-table :data="testDatas" border style="width: 100%;margin-top:10px" @header-contextmenu="colRightClick">
-	<el-table-column v-if="testCols.length > 0" type="index" :label="'编号'" :width="50"></el-table-column>
-  <el-table-column v-for="(column, idx) in testCols" :key="idx" :index="idx">
-		<!--label-->
-  	<template slot="header" slot-scope="scope1">
-  		<p v-show="column.show" @dblclick="column.show=false">
-  			{{column.txt}} 
-  			<i class="el-icon-edit-outline" @click="column.show=false"></i>
-  		</p>
-  		<el-input
-  			size="mini"
-  			v-show="!column.show"
-  			v-model="column.txt"
-  			@blur="column.show=true">
-  		</el-input>
-    </template>
-		<!--prop-->
-  	<template slot-scope="scope">
-  		<p v-show="scope.row[column.col].show" @dblclick="scope.row[column.col].show=false">
-  			{{scope.row[column.col].content}} 
-  			<i class="el-icon-edit-outline" @click="scope.row[column.col].show=false"></i>
-  		</p>
-  		<el-input type="textarea" :autosize="{minRows:2,maxRows:4}"
-  			v-show="!scope.row[column.col].show"
-  			v-model="scope.row[column.col].content"
-  			@blur="scope.row[column.col].show=true">
-  		</el-input>
-  	</template>
-  </el-table-column>
-</el-table>
+<div id="hello">
+    <h4 style="display: inline-block;margin:0;">测试表</h4>
+    <div style="display: inline-block;float: right;">
+        <el-button size="mini" type="primary" @click="consoleDatas">打印数据</el-button>
+        <el-button size="mini" type="primary" @click="addRow">增加行</el-button>
 
-		<div v-show="showMenu" id="contextmenu">
-			<i class="el-icon-circle-close hideContextMenu" @click="showMenu=false"></i>
-			<el-button size="mini" type="primary" @click="addColumn(curColumn)">前方插入一列</el-button>
-			<el-button size="mini" type="primary" @click="addColumn(curColumn+1)">后方插入一列</el-button>
-			<el-popconfirm title="确定删除该列吗？" @onConfirm="delColumn">
-				<el-button slot="reference" type="primary" size="mini">删除当前列</el-button>
-			</el-popconfirm>
-		</div>
+        <el-button @click="delLastRow" slot="reference" type="primary" size="mini">删除末行</el-button>
 
-  </div>
+    </div>
+    <el-table :data="testDatas" border style="width: 100%;margin-top:10px" @header-contextmenu="colRightClick">
+        <el-table-column v-if="testCols.length > 0" type="index" :label="'编号'" :width="50"></el-table-column>
+        <el-table-column v-for="(column, idx) in testCols" :key="idx" :index="idx">
+            <!--label-->
+            <template slot="header" slot-scope="scope1">
+                <p v-show="column.show" @dblclick="column.show=false">
+                    {{column.txt}}
+                    <i class="el-icon-edit-outline" @click="column.show=false"></i>
+                </p>
+                <el-input size="mini" v-show="!column.show" v-model="column.txt" @blur="column.show=true">
+                </el-input>
+            </template>
+            <!--prop-->
+            <template slot-scope="scope">
+                <p v-show="scope.row[column.col].show" @dblclick="scope.row[column.col].show=false">
+                    {{scope.row[column.col].content}}
+                    <i class="el-icon-edit-outline" @click="scope.row[column.col].show=false"></i>
+                </p>
+                <el-input type="textarea" :autosize="{minRows:2,maxRows:4}" v-show="!scope.row[column.col].show" v-model="scope.row[column.col].content" @blur="scope.row[column.col].show=true">
+                </el-input>
+            </template>
+        </el-table-column>
+    </el-table>
+
+
+</div>
 </template>
 
 <script>
 export default {
-      data(){
-	 return{
-			testCols: [
-				{col: "name", txt: 'name', show: true},
-				{col: "age", txt: 'age', show: true},
-				{col: "city", txt: 'city', show: true},
-				{col: "tel", txt: 'tel', show: true}
-			],
-			testDatas: [{
-					name: {content: '张三', show: true},
-					age: {content: 24, show: true},
-					city: {content: '广州', show: true},
-					tel: {content: '13312345678', show: true}
-				},{
-					name: {content: '李四', show: true},
-					age: {content: 25, show: true},
-					city: {content: '九江', show: true},
-					tel: {content: '18899998888', show: true}
-				}
-			],
-			count_col: 0,
-			showMenu: false,
-			curColumn: null,
-         	//systemInfo:{
+    data() {
+        return {
+            testCols: [
+                { col: "name", txt: '项目', show: true },
+                { col: "unit", txt: '单位', show: true },
+                { col: "value", txt: '数值', show: true },
+                { col: "note", txt: '备注', show: true }
+            ],
+            testDatas: [{
+                name: { content: '电', show: true },
+                unit: { content: 'kW', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            }, {
+                name: { content: '标准燃料', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '净化水', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '新鲜水', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '循环水', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '净化水', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '除盐水', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '除氧水', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '工艺凝结水', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '透平凝结水', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '污水', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '3.5Mpa蒸汽', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '1.0Mpa蒸汽', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '0.4Mpa蒸汽', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '净化风', show: true },
+                unit: { content: 'Nm^3/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '非净化风', show: true },
+                unit: { content: 'Nm^3/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '间隔用量', show: true }
+            },{
+                name: { content: '氨气', show: true },
+                unit: { content: 'Nm^3/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            },{
+                name: { content: '胺液', show: true },
+                unit: { content: 't/h', show: true },
+                value: { content: '', show: true },
+                note: { content: '', show: true }
+            }],
+            count_col: 0,
+            showMenu: false,
+            curColumn: null,
+            //systemInfo:{
             //    .....
             //}
-	 }
-  },
+        }
+    },
     created: function () {
         console.log("turn to system page");
     },
     methods: {
-        colRightClick(column,event) {
-			window.event.returnValue = false; //阻止浏览器自带的右键菜单弹出
-			if(!column.index && column.index !== 0) return;
-			this.curColumn = column.index
-			this.showMenu = true
-			var ele = document.getElementById('contextmenu')
-			ele.style.top = event.clientY + 'px';
-			ele.style.left = event.clientX + 'px';
-			if(window.innerWidth - 140 < event.clientX) {
-				ele.style.left = 'unset'
-				ele.style.right = 0
-			}
-		},
-		addRow() { // 新增行
-			this.showMenu = false
-			var obj = {}
-			this.testCols.map(p => {
-				obj[p.col] = {content: '', show: true}
-			})
-			this.testDatas.push(obj)
-		},
-		// 当row中存在一“主键”可唯一标识row的下标时（如：编号放在testDatas内），可借此实现行的自由插入与删除
-		addColumn(idx) { // 新增列
-			this.showMenu = false
-			var obj = {col: 'col_' + this.count_col++, txt: '', show: true}
-			if(idx || idx === 0) this.testCols.splice(idx, 0, obj);
-			else this.testCols.push(obj);
-			var _this = this
-			this.testDatas.map(p => { // 新增的对象无法被vue监听到
-				_this.$set(p, obj.col, {content: '', show: true})
-//				p[obj.col] = {content: '', show: true}
-			})
-		},
-		delColumn() { // 删除列
-			this.showMenu = false
-			var colKey = this.testCols[this.curColumn].col;
-			this.testCols.splice(this.curColumn, 1);
-			this.testDatas.map(p => {
-				delete p[colKey];
-			});
-		},
-		delLastRow() { // 删除行
-			this.showMenu = false
-			var len = this.testDatas.length;
-			if(len > 0) this.testDatas.splice(len - 1, 1);
-			else this.$message.error('没有可删除行');
-		},
-		consoleDatas() {
-			console.log('表头',this.testCols);
-			console.log('数据',this.testDatas);
-		}
-	}
-    },
+        colRightClick(column, event) {
+            window.event.returnValue = false; //阻止浏览器自带的右键菜单弹出
+            if (!column.index && column.index !== 0) return;
+            this.curColumn = column.index
+            this.showMenu = true
+            var ele = document.getElementById('contextmenu')
+            ele.style.top = event.clientY + 'px';
+            ele.style.left = event.clientX + 'px';
+            if (window.innerWidth - 140 < event.clientX) {
+                ele.style.left = 'unset'
+                ele.style.right = 0
+            }
+        },
+        addRow() { // 新增行
+            this.showMenu = false
+            var obj = {}
+            this.testCols.map(p => {
+                obj[p.col] = { content: '', show: true }
+            })
+            this.testDatas.push(obj)
+        },
 
+        delLastRow() { // 删除行
+            this.showMenu = false
+            var len = this.testDatas.length;
+            if (len > 0) this.testDatas.splice(len - 1, 1);
+            else this.$message.error('没有可删除行');
+        },
+        consoleDatas() {
+            console.log('表头', this.testCols);
+            console.log('数据', this.testDatas);
+        }
+    }
 }
 </script>
