f69ad571fd6710f334bc14295e480f540f05fe2e
l20192020 <lyq20070185@126.com>
2020-11-18 17:13:24 +0800

    催化重整装置前端 产品性质修改

--- FrontEnd/src/views/CR/product.vue
+++ FrontEnd/src/views/CR/product.vue
@@ -1,61 +1,38 @@
 <template>
 <div id="hello">
     <div id="hello">
-        <h4 style="display: inline-block;margin:0;">产品性质</h4>
+        
         <div style="display: inline-block;float: right;">
             <el-button v-if="showTable" size="mini" type="primary" @click="consoleDatas">打印数据</el-button>
             <el-button v-if="showTable" size="mini" type="primary" @click="addRow">增加行</el-button>
             <el-button v-if="showTable" @click="delLastRow" slot="reference" type="primary" size="mini">删除末行</el-button>
         </div>
         <br><br>
-        <div>
-            粘度温度点（℃）
-            <el-select v-model="viscosity_t" multiple filterable allow-create :disabled="showTable" placeholder="请选择温度">
-                <el-option label="20" value=20></el-option>
-                <el-option label="40" value=40></el-option>
-            </el-select>
-            <el-button type="primary" @click="refreshTable">创建表格</el-button>
-        </div>
-        <el-table v-if="showTable" :data="productDatas" border style="width: 100%;margin-top:10px" max-height="500">
-            <el-table-column v-if="productCols.length > 0" type="index" :label="'编号'" :width="50"></el-table-column>
-            <!-- 固定列 -->
-            <el-table-column v-for="(column, idx) in productCols" :key="idx" :index="idx" :width="160">
-                <!--label-->
-                <template slot="header" slot-scope="scope1">
-                    <p>
-                        {{column.txt}}
-                    </p>
-                </template>
-                <!--prop-->
-                <template slot-scope="scope">
-                    <p v-show="scope.row[column.col].show" @dblclick="scope.row[column.col].show=false">
-                        {{scope.row[column.col].content}}
-                        <i class="el-icon-edit-outline" @click="scope.row[column.col].show=false"></i>
-                    </p>
-                    <el-input type="textarea" :autosize="{minRows:2,maxRows:4}" v-show="!scope.row[column.col].show" v-model="scope.row[column.col].content" @blur="scope.row[column.col].show=true">
-                    </el-input>
-                </template>
-            </el-table-column>
-            <!-- 运动粘度 -->
-            <el-table-column v-if="viscosity_t.length>0" label="运动粘度（m㎡/s）">
-                <el-table-column v-for="(column, idx) in viscosity_t" :key="idx" :index="idx">
+        <div style="position:relative;left:500px;">
+            <el-table v-if="true" :data="productDatas" border style="width:550px;margin-top:10px;" max-height="500" >
+                <el-table-column v-if="productCols.length > 0" type="index" :label="'编号'" :width="50"></el-table-column>
+                <!-- 固定列 -->
+                <el-table-column v-for="(column, idx) in productCols" :key="idx" :index="idx" :width="160">
                     <!--label-->
                     <template slot="header" slot-scope="scope1">
                         <p>
-                            {{column}}℃
+                            {{column.txt}}
                         </p>
                     </template>
+                    <!--prop-->
                     <template slot-scope="scope">
-                        <p v-show="scope.row['vis'+column].show" @dblclick="scope.row['vis'+column].show=false">
-                            {{scope.row['vis'+column].content}}
-                            <i class="el-icon-edit-outline" @click="scope.row['vis'+column].show=false"></i>
+                        <p v-show="scope.row[column.col].show" @dblclick="scope.row[column.col].show=false">
+                            {{scope.row[column.col].content}}
+                            <i class="el-icon-edit-outline" @click="scope.row[column.col].show=false"></i>
                         </p>
-                        <el-input type="textarea" :autosize="{minRows:2,maxRows:4}" v-show="!scope.row['vis'+column].show" v-model="scope.row['vis'+column].content" @blur="scope.row['vis'+column].show=true">
+                        <el-input type="textarea" :autosize="{minRows:2,maxRows:4}" v-show="!scope.row[column.col].show" v-model="scope.row[column.col].content" @blur="scope.row[column.col].show=true">
                         </el-input>
                     </template>
                 </el-table-column>
-            </el-table-column>
-        </el-table>
+                <!-- 运动粘度 -->
+
+            </el-table>
+        </div>
         <br>
         <el-button type="primary" @click="addToStore">暂存此页</el-button>
     </div>
@@ -69,13 +46,8 @@ export default {
             showTable: false,
             viscosity_t: [],
             productCols: [{ col: "product_name", txt: '产品名称' },
-                { col: "density", txt: '密度(20℃) （kg/m³）' },
-                { col: "API", txt: '比重指数(API)' },
-                { col: "molecular_weight", txt: '分子量' },
-                { col: "characteristic_factor", txt: '特性因数' },
-                { col: "sulfur_content", txt: '含硫量（wt/%）' },
-                { col: "acid_value", txt: '酸值（mgKOH/g）' },
-                { col: "remarks", txt: '备注' }
+                { col: "output", txt: '数量 （万吨/年）' },
+                { col: "purpose", txt: '去向或用途' },
             ],
             productDatas: [],
             count_col: 0,
@@ -86,9 +58,8 @@ export default {
 
     created: function () {
         console.log("turn to system page");
-        var items = ["初顶气", "初顶油", "常顶气", "常顶油", "常一线", "常二线", "常三线", "常四线", "常压渣油（常底油）",
-            "贫吸收油", "常压重油", "过汽化油", "减顶气", "减顶油", "减一线", "减二线", "减三线", "减四线", "减五线",
-            "减六线", "减压渣油"
+        var items = ["液化气", "重整汽油", "苯", "拔头油", "含氢气体", "含氢气体中纯氢", "燃料气", "C6馏分", "戊烷油",
+            "含硫燃料气", "高辛烷值重整汽油", "抽余油"
         ];
         for (var item in items) {
             this.addRow();
